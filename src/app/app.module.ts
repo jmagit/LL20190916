5c07850a1ceb7a1570463b79caf723adf2a0aeab
Javier <jmartin@grupoloyal.com>
2019-09-16 18:06:50 +0200

    servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndraCoreModule } from 'src/indra-core';
+import { MainModule } from './main';
+import { SecurityModule } from './security';
+import { AppCommonModule } from './app-common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule,
+    IndraCoreModule, MainModule, SecurityModule, AppCommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
