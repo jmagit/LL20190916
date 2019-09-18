7d81a894669f88b3638494eeee99829bc8cf2aaa
Javier <jmartin@grupoloyal.com>
2019-09-18 12:58:37 +0200

    Ejer

--- src/indra-core/index.ts
+++ src/indra-core/index.ts
@@ -1,3 +1,4 @@
 export { IndraCoreModule } from './indra-core.module';
 export * from './services/logger.service';
 export { ElipsisPipe, CapitalizePipe, StripTagsPipe } from './pipes/cadenas.pipe';
+export { ToComaDecimalPipe, LocalDecimalPipe } from './pipes/numericos.pipe';
